097e1a155b832b797ef3037a15bffb6908792ffd
zhangrh <zhangrunhao@jingdata.com>
2018-11-19 16:29:00 +0800

    添加有状态的类RandomState

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -15,9 +14,22 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
           // child: new Text('Hello World'),
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
